215a7b4f21300e38ea38679ab2c677fbbe1a2107
callmewaggs <zxv74014@gmail.com>
2020-03-22 16:46:36 +0900

    chore: initial commit
     - create project for Java, Gradle
     - add Java, Java-Web, Gradle, IntelliJ setting to .gitignore
     - add Spring JPA dependency

---
+++ src/main/java/com/github/callmewaggs/decenttownapi/DecentTownApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.callmewaggs.decenttownapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentTownApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DecentTownApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/callmewaggs/decenttownapi/DecentTownApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.callmewaggs.decenttownapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentTownApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
